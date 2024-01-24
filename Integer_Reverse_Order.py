285a4ad8021fea0d96c3989e819f37a1638a73f4
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-24 22:17:31 +0800

    Coverted the integer to a string, reversed it, and iterated through each other

--- Integer_Reverse_Order.py
+++ Integer_Reverse_Order.py
@@ -1,4 +1,9 @@
 # Write a program to extract each digit from an integer in the reverse order.
+
 # pseudocode
+
 # Convert the integer to a string, reverse it, and then iterate through each character
+def reverse_digits(integer):
+    reverse_digits=[char for char in str(integer)[::-1]]
+    
 # Print the reversed digits with a space separating them

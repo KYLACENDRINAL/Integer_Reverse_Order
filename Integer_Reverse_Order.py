0749d0741fa0dc56ddd15000e00ad426417fded7
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-24 21:53:16 +0800

    pseudocode

---
+++ Integer_Reverse_Order.py
@@ -0,0 +1,4 @@
+# Write a program to extract each digit from an integer in the reverse order.
+# pseudocode
+# Convert the integer to a string, reverse it, and then iterate through each character
+# Print the reversed digits with a space separating them
